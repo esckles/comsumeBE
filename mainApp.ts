a0e87e6d95212cfdadeb5a12da05a3fa5c29ce58
esckles <davidsamzzywilliams1234@gmail.com>
2025-01-15 10:52:30 +0100

    env

--- mainApp.ts
+++ mainApp.ts
@@ -1,8 +1,8 @@
 import { Application, Request, Response } from "express";
-// import AuthRouter from "./router/AuthRouter";
+import AuthRouter from "./router/AuthRouter";
 export const mainApp = async (app: Application) => {
   try {
-    // app.use("/api", AuthRouter);
+    app.use("/api", AuthRouter);
     app.get("/", (req: Request, res: Response) => {
       res.status(200).json({ message: "Welcome to my API", status: 200 });
     });
